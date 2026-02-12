26571c2bb979fdea13991fa33ff9b7d8623507e0
Fru1t2 <tjrlcjf04@naver.com>
2026-02-13 00:25:07 +0900

    bug fix

--- agent/quick_start_vision.py
+++ agent/quick_start_vision.py
@@ -1,17 +1,3 @@
-import builtins
-
-def debug_add(self, other):
-    if other is None and isinstance(self, int):
-        import traceback
-        import sys
-        print("\n" + "!"*60)
-        print("🚨 [CRITICAL DEBUG] int + None 발생 감지!")
-        traceback.print_stack(file=sys.stdout)
-        print("!"*60 + "\n")
-        raise TypeError("STOP HERE: Found the culprit!")
-    return original_add(self, other)
-
-
 from main import run_agent
 
 # run a example for vision tasks. save the execution trace to outputs/blink_spatial
